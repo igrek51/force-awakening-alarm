f1a04d057cb3939fe703ae92792a7a4f0fde96c9
igrek51 <igrek.s@o2.pl>
2018-05-26 16:15:16 +0200

    Playing ringtone alarm even if device is muted

--- app/src/main/java/igrek/forceawaken/AwakenActivity.java
+++ app/src/main/java/igrek/forceawaken/AwakenActivity.java
@@ -1,11 +1,21 @@
 package igrek.forceawaken;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.io.IOException;
+
 public class AwakenActivity extends AppCompatActivity {
+	
+	Ringtone ringtone;
+	MediaPlayer mMediaPlayer;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +29,52 @@ public class AwakenActivity extends AppCompatActivity {
 		win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
 	}
 	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		
+		// TUrn the sound ON !
+		//		if (am.getRingerMode() != AudioManager.RINGER_MODE_NORMAL)
+		//			am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+		
+		//		am.setStreamVolume(AudioManager.STREAM_RING, am.getStreamMaxVolume(AudioManager.STREAM_RING), AudioManager.FLAG_PLAY_SOUND);
+		// someone could turned this off
+		am.setStreamVolume(AudioManager.STREAM_ALARM, am.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+		
+		//		Uri ringUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+		Uri ringUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sweetwater);
+		//		ringtone = RingtoneManager.getRingtone(getApplicationContext(), ringUri);
+		//		ringtone.play();
+		
+		try {
+			mMediaPlayer = new MediaPlayer();
+			mMediaPlayer.setDataSource(getApplicationContext(), ringUri);
+			
+			//			if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+			mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+			mMediaPlayer.setVolume(1, 1);
+			mMediaPlayer.prepare();
+			mMediaPlayer.start();
+			//			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//		MediaPlayer mMediaPlayer = new MediaPlayer();
+		//		mMediaPlayer.setDataSource(this, ringUri);
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (ringtone != null) {
+			ringtone.stop();
+		}
+		if (mMediaPlayer != null) {
+			mMediaPlayer.stop();
+		}
+	}
 }
