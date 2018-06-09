7fa1abfe5577ec3ab3fb07e85803398f19207022
igrek51 <igrek.s@o2.pl>
2018-06-09 13:12:01 +0200

    Fix Noise Detector Test

--- app/src/main/java/igrek/forceawaken/domain/ringtone/Ringtone.java
+++ app/src/main/java/igrek/forceawaken/domain/ringtone/Ringtone.java
@@ -4,15 +4,23 @@ import android.net.Uri;
 
 import java.io.File;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class Ringtone {
 	
 	private File file;
 	private String name;
+	
+	public Ringtone(File file, String name) {
+		this.file = file;
+		this.name = name;
+	}
+	
+	public File getFile() {
+		return file;
+	}
+	
+	public String getName() {
+		return name;
+	}
 	
 	public Uri getUri() {
 		return Uri.fromFile(file);
--- app/src/test/java/igrek/forceawaken/service/noise/NoiseDetectorTest.java
+++ app/src/test/java/igrek/forceawaken/service/noise/NoiseDetectorTest.java
@@ -1,15 +1,34 @@
 package igrek.forceawaken.service.noise;
 
+import org.junit.Before;
 import org.junit.Test;
 
-//import static org.assertj.core.api.Assertions.assertThat;
+import igrek.forceawaken.MainActivity;
+import igrek.forceawaken.dagger.DaggerIOC;
+import igrek.forceawaken.dagger.test.BaseDaggerTest;
 
-public class NoiseDetectorTest {
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class NoiseDetectorTest extends BaseDaggerTest {
+	
+	@Before
+	public void setUp() {
+		MainActivity activity = mock(MainActivity.class);
+		// Dagger init test
+		DaggerIOC.initTest(activity);
+		DaggerIOC.getTestComponent().inject(this);
+	}
 	
 	@Test
 	public void testCalculateAlarmVolume() {
-		NoiseDetectorService noiseDetectorService = new NoiseDetectorService();
-		//		assertThat(noiseDetectorService.calculateAlarmVolume(0)).isEqualTo(noiseDetectorService.noiseVolTransform[1]);
+		NoiseDetectorService nd = new NoiseDetectorService();
+		assertThat(nd.calculateAlarmVolume(0.0)).isEqualTo(nd.noiseVolTransform[1]);
+		assertThat(nd.calculateAlarmVolume(nd.noiseVolTransform[0])).isEqualTo(nd.noiseVolTransform[1]);
+		assertThat(nd.calculateAlarmVolume(nd.noiseVolTransform[2])).isEqualTo(nd.noiseVolTransform[3]);
+		double middle = (nd.noiseVolTransform[0] + nd.noiseVolTransform[2]) / 2;
+		assertThat(nd.calculateAlarmVolume(middle)).isGreaterThan(nd.noiseVolTransform[1])
+				.isLessThan(nd.noiseVolTransform[3]);
 	}
 	
 }
